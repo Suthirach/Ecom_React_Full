d5ba558329711e8b8eb4a35d2164b7de412e5f39
Suthirach <145276729+Suthirach@users.noreply.github.com>
2025-01-09 01:51:27 +0700

    โครงสร้าง jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+
+const App = () => {
+  //js
+  return (
+    <>
+      <AppRoutes/>
+    </>
+  )
+};
+console.log(AppRoutes)
+export default App;
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,68 @@
+// rafce 
+import React from 'react'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import  Home  from '../pages/Home'
+import  Shop  from '../pages/Shop'
+import  Cart  from '../pages/Cart'
+import  History  from '../pages/History'
+import  HomeUser  from '../pages/user/HomeUser'
+import  Login  from '../pages/auth/Login'
+import  Register from '../pages/auth/Register'
+import  Category  from '../pages/admin/Category'
+import  DashBoard  from '../pages/admin/DashBoard'
+import  Product  from '../pages/admin/Product'
+import Checkout from '../pages/Checkout'
+import Layout from '../layouts.jsx/Layout'
+import LayoutAdmin from '../layouts.jsx/LayoutAdmin'
+import LayoutUser from '../layouts.jsx/LayoutUser'
+import Manage from '../pages/admin/Manage'
+
+
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Layout/>,
+        children:[
+          { index: true, element: <Home />},
+          { path: 'shop', element: <Shop /> },
+          { path: 'cart', element: <Cart />},
+          { path: 'history', element: <History />},
+          { path: 'checkout', element: <Checkout />},
+          { path: 'login', element: <Login />},
+          { path: 'register', element: <Register/>},
+        ]
+      },
+      {
+        path: '/admin',
+        element:<LayoutAdmin/>,
+        children:[
+            { index: true, element: <DashBoard />},
+            { path: 'category', element: <Category />},
+            { path: 'product', element: <Product />}, 
+            { path: 'manage', element: <Manage />} 
+            
+          ]
+        },
+      {
+        path: '/user',
+        element:<LayoutUser/>,
+        children:[
+          { index:true, element: <HomeUser />},
+
+        ]
+
+      }, 
+
+])  
+
+console.log
+const AppRoutes = () => {
+  return (
+    <>
+        <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default AppRoutes
